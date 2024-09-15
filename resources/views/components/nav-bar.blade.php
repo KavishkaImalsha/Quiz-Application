fb00ee1c6db29981432eac687eee4e7c7fdf6705
Kavishka <kavishkaimalsha@gmail.com>
2024-09-15 12:23:08 +0530

    Create nav-bar component

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,37 @@
+<nav class="bg-blue-500 grid grid-cols-2">
+    <div class="w-16 h-16 flex ml-2">
+        <img src="{{asset("images/logo.jpeg")}}" alt="logo" class="rounded-full flex">
+        <h3 class="flex pt-4 pl-2 text-white font-bold text-3xl">Smarty <span class="text-amber-400">Quiz</span></h3>
+    </div>
+
+    <div class="flex justify-end pt-3 font-bold">
+        @if (Route::has('login'))
+            @auth
+                <a
+                    href="{{ url('/dashboard') }}"
+                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                >
+                    Dashboard
+                </a>
+            @else
+                <a
+                    href="{{ route('login') }}"
+                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                >
+                    Log in
+                </a>
+
+                @if (Route::has('register'))
+                    <a
+                        href="{{ route('register') }}"
+                        class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Register
+                    </a>
+                @endif
+            @endauth
+        @endif
+    </div>
+
+
+</nav>
