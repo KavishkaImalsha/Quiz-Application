6364d2c8078ae32b94a78f63177d286becc9dc0d
Kavishka <kavishkaimalsha@gmail.com>
2024-09-16 17:11:41 +0530

    Change login routes according to user role and make temp design for user dashboard

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+    <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>user dashboard</title>
+</head>
+<body>
+<form method="POST" action="{{ route('logout') }}">
+    @csrf
+
+    <x-dropdown-link :href="route('logout')"
+                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+        {{ __('Log Out') }}
+    </x-dropdown-link>
+</form>
+
+    <div>This is user dashboard</div>
+</body>
+</html>
