da54a449d8046646091ca6067aad5665aa341c88
Kavishka <kavishkaimalsha@gmail.com>
2024-09-17 23:38:21 +0530

    Make migration for quizzes and courses table and make models and controllers related to tables

---
+++ app/Models/quiz.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class quiz extends Model
+{
+    use HasFactory;
+
+    protected $table = 'quizzes';
+
+    protected $fillable = [
+        'course_id',
+        'quiz',
+        'choices'
+    ];
+}
